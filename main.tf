ad4b95fb8ca70b2fa11e6f2f8d208b81488ad698
vivekmishra3712 <vivekmishra3712@gmail.com>
2024-10-01 15:16:23 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,44 @@
+provider "google" {
+  project = var.project_id
+  region  = var.region
+}
+
+resource "google_container_cluster" "primary" {
+  name     = var.cluster_name
+  location = var.location
+
+  initial_node_count = 1
+
+  node_config {
+    machine_type = "e2-medium"
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/cloud-platform",
+    ]
+    service_account = var.service_account
+    
+    # Set the disk size to 200GB
+    disk_size_gb = 200
+  }
+
+  deletion_protection = false 
+}
+
+variable "project_id" {
+  description = "The ID of the GCP project"
+}
+
+variable "region" {
+  description = "The GCP region"
+}
+
+variable "location" {
+  description = "The GCP zone or location"
+}
+
+variable "cluster_name" {
+  description = "The name of the GKE cluster"
+}
+
+variable "service_account" {
+  description = "The service account for the GKE cluster"
+}
